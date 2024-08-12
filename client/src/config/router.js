d5ac04dc675fdef66888ae1e68d769e662d9d0b0
mrinaljain <jain.mrinal140@gmail.com>
2024-08-12 21:26:56 +0530

    test random page

--- client/src/config/router.js
+++ client/src/config/router.js
@@ -1,14 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App.js";
 import Visitor from "../pages/Visitor.js";
+import FourOFour from "../pages/FourOFour.js";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    // children: [{ path: "/:link", element: <Visitor /> }],
   },
   { path: ":link", element: <Visitor /> },
-  { path: "visit/:link", element: <Visitor /> },
+  // { path: "visit/:link", element: <Visitor /> },
+  { path: "/test", element: <FourOFour /> },
 ]);
 
 export default router;
---
+++ client/src/pages/FourOFour.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function FourOFour() {
+  return <div>FourOFour</div>;
+}
+
+export default FourOFour;
